2fdb0293479bfbba9f0583909febc89cbef301f2
Damiano <pozzatodamiano@gmail.com>
2023-10-02 15:31:32 +0200

    getter/setter Users

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,43 @@
 package model;
 
 public class User {
+    private String name;
+    private String surname;
+    private String nickname;
+    private String date; //TODO: aggiornare
+    private String password;
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
